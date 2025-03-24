73882f2049058e47ed8eb80cc828a828007782ea
navitadamayantisyarif <navitadamayanti@gmail.com>
2025-03-24 22:52:52 +0700

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){ 
+/* ---------------------------------------------------------------------- */
+/*	Typing Effect
+/* ---------------------------------------------------------------------- */
+var typed = new Typed('#typed', {
+	strings: ["Welcome to my portfolio.", "I'm a junior web developer.", "I'm Navita Damayanti Syarif."],
+	typeSpeed: 100,
+	startDelay: 150,
+	loop: true
+  });	 
+});
